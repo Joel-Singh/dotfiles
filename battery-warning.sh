f715977c1b5d0c70e79c9221f17c644f960db929
Joel Singh <joelsingh788@gmail.com>
2025-01-15 11:11:37 -0500

    Change battery warning to 30%

--- battery-warning.sh
+++ battery-warning.sh
@@ -4,8 +4,8 @@
 while true; do
   battery_level=$(acpi -b | awk '/^Battery 0/ {print $4}')
   battery_level=${battery_level//[%,]}
-  if [ "$battery_level" -lt 50 ]; then
-    notify-send "Battery Warning" "Battery level is below 50%"
+  if [ "$battery_level" -lt 30 ]; then
+    notify-send "Battery Warning" "Battery level is below 30%"
   fi
   sleep 1
 done
