16c3245368f0f9a1c0780ad46b35846ee7783650
Joel Singh <joelsingh788@gmail.com>
2025-01-16 10:55:10 -0500

    Only do battery warning if discharging

--- battery-warning.sh
+++ battery-warning.sh
@@ -4,7 +4,10 @@
 while true; do
   battery_level=$(acpi -b | awk '/^Battery 0/ {print $4}')
   battery_level=${battery_level//[%,]}
-  if [ "$battery_level" -lt 30 ]; then
+
+  discharging=$(acpi -b | awk '/Discharging/ {print $3}')
+
+  if [ "$battery_level" -lt 30 ] && [ -n "$discharging" ]; then
     notify-send "Battery Warning" "Battery level is below 30%"
   fi
   sleep 1
