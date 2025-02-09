69223940516e7cba031a46b572f817b09c49424f
Joel Singh <joelsingh788@gmail.com>
2025-02-08 19:28:29 -0500

    Change battery warning to 15

--- battery-warning.sh
+++ battery-warning.sh
@@ -7,8 +7,8 @@ while true; do
 
   discharging=$(acpi -b | awk '/^Battery 0: Discharging/ {print $3}')
 
-  if [ "$battery_level" -lt 15 ] && [ -n "$discharging" ]; then
-    notify-send "Battery Warning" "Battery level is below 15%"
+  if [ "$battery_level" -lt 20 ] && [ -n "$discharging" ]; then
+    notify-send "Battery Warning" "Battery level is below 20%"
   fi
   sleep 1
 done
